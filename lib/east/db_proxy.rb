0fbac502dbc71ac59e13588c7eb63bef397ea601
ericluo <eric.wenbl@gmail.com>
2014-04-18 19:28:51 +0800

    update db proxy

--- lib/east/db_proxy.rb
+++ lib/east/db_proxy.rb
@@ -1,11 +1,19 @@
+require 'open3'
+
 module East
   module DBProxy
+    DB2 = "/home/db2inst1/sqllib/bin/db2"
+    
     def psql(opts, input)
       IO.popen("psql #{opts}", 'r+') do |io|
         io.puts input
         io.close_write
         return io.read
       end
+    end
+
+    def db2(opts, input)
+      Open3.popen2e("db2 #{opts}")
     end
   end
 end
--- spec/east/db_proxy_spec.rb
+++ spec/east/db_proxy_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 module East
-  include DBProxy
-  it "psql command will connect to pg database and execute sql command" do
-    if mac?                   # os guard because psql installed only on macosx 
-      psql("testdb ", "select 1 + 1;select current_date;").must_match "2"
-      puts psql "testdb", <<-end
+  describe DBProxy do
+    include DBProxy
+    it "psql command will connect to pg database and execute sql command" do
+      if mac?                   # os guard because psql installed only on macosx 
+        psql("testdb ", "select 1 + 1;select current_date;").must_match "2"
+        puts psql "testdb", <<-end
         select 1 + 1;
         select current_date;
+      end
       end
     end
   end
